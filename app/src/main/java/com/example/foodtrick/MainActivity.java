f151eb8ba7d8b112a2eb92aa224fee54ff1efed7
Fonsi20 <fonsi20fernandez@gmail.com>
2019-02-12 00:07:12 +0100

    Primer Commit

--- app/src/main/java/com/example/foodtrick/MainActivity.java
+++ app/src/main/java/com/example/foodtrick/MainActivity.java
@@ -1,12 +1,7 @@
 package com.example.foodtrick;
 
-import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
-import uk.co.chrisjenx.calligraphy.CalligraphyContextWrapper;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,16 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // añadimos las fuentes
-        CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
-                .setDefaultFontPath("fonts/MontserratRegular.ttf")
-                .setFontAttrId(R.attr.fontPath)
-                .build()
-        );
-    }
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        super.attachBaseContext(CalligraphyContextWrapper.wrap(newBase));
     }
 }
